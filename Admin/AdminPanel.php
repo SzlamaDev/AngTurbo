a46e6e5eacd32393e16601d5deaed3a8b9c07587
Miłosz szlama <milosz.szlama@szlamadev.pl>
2024-06-12 11:27:22 +0200

    update AdminPanel.php

--- Admin/AdminPanel.php
+++ Admin/AdminPanel.php
@@ -103,7 +103,7 @@
             echo "Pola nie moga byc puste!";
         }
         ?>
-        <h3>Lista słówek:</h3>
+        <h3>Lista słówek / usuń słówko:</h3>
         <form method="post" action="AdminPanel.php">
             <select id="showcategory" name="showcategory">
                 <?php
@@ -147,7 +147,6 @@
             echo "<tr>" . "<td>" . $row[0] . "</td>" . "<td>" . $row[1] . "</td>" . "<td>" .  $row[2] . "</td>" . "<td>" . $row[3] . "</td>" . "<td>" . $row[4] . "</td>" . "<td>" . $row[5] . "</td>" . "<td>" . $row[6] . "</td>" . "</tr>";
         }
         ?>
-    <h3>Usuń słówko:</h3>
     <form action="AdminPanel.php" METHOD="post">
         <label for="wordnumber">Identyfikator słowka do usunięcia: </label>
         <input type="number" name="wordnumber" id="wordnumber">
@@ -157,7 +156,88 @@
     @$wordnumber = $_POST["wordnumber"];
     mysqli_query($polaczenie, "Delete from words where id = '$wordnumber';");
     ?>
-<h3>Usuń Ucznia: </h3>
+    <h3>Dodaj / Usuń kategorię: </h3>
+    <form method="post" action="AdminPanel.php">
+        <label for="categorystudent">Uczeń:</label>
+        <select id="categorystudent" name="categorystudent">
+            <?php
+            $zapytanie = $polaczenie->query("SELECT id, username from student;");
+            while ($row = mysqli_fetch_array($zapytanie)){
+                echo "<option value='$row[0]'>" . $row[1]; "</option>";
+            }
+            ?>
+        </select><br />
+        <label for="categoryname">Nazwa kategorii:</label>
+        <input type="text" name="categoryname" id="categoryname">
+        <input type="submit" value="Dodaj">
+    </form>
+    <?php
+    @$categorystudent = $_POST["categorystudent"];
+    @$categoryname = $_POST["categoryname"];
+    $zapytanie = $polaczenie->query("SELECT parent_id from student where id = '$categorystudent';");
+    while ($row = mysqli_fetch_array($zapytanie)){
+        @$parent_id = $row[0];
+    }
+    if (!empty($categoryname)) {
+        mysqli_query($polaczenie, "Insert into category(parent_id, student_id, name) values ('$parent_id', '$categorystudent', '$categoryname');");
+    }
+    else{
+        echo "Pola nie moga byc puste!";
+    }
+    ?>
+    <table>
+        <tr>
+            <th>
+                ID
+            </th>
+            <th>
+                Nazwa
+            </th>
+        </tr>
+        <?php
+        $zapytanie = $polaczenie->query("SELECT id, name from category;");
+        while ($row = mysqli_fetch_array($zapytanie)){
+            echo "<tr>" . "<td>" . $row[0] . "</td>" . "<td>" . $row[1] . "</td>" . "</tr>";
+        }
+        ?>
+    </table>
+    <form action="AdminPanel.php" METHOD="post">
+        <label for="categorynumber">Identyfikator Kategorii do usunięcia: </label>
+        <input type="number" name="categorynumber" id="categorynumber">
+        <input type="submit" value="usuń">
+    </form>
+    <?php
+    @$categorynumber = $_POST["categorynumber"];
+    mysqli_query($polaczenie, "Delete from category where id = '$categorynumber';");
+    ?>
+<h3>Dodaj / Usuń Ucznia: </h3>
+    <form method="post" action="AdminPanel.php">
+        <label for="addingstudent">Rodzic:</label>
+        <select id="addingstudent" name="addingstudent">
+            <?php
+            $zapytanie = $polaczenie->query("SELECT id, username from users");
+            while ($row = mysqli_fetch_array($zapytanie)){
+                echo "<option value='$row[0]'>" . $row[1]; "</option>";
+            }
+            ?>
+        </select><br />
+        <label for="studentlogin">Login:</label>
+        <input type="text" id="studentlogin" name="studentlogin"><br />
+        <label for="studentpasswd">Hasło:</label>
+        <input type="password" id="studentpasswd" name="studentpasswd">
+        <input type="submit" value="Dodaj">
+    </form>
+    <?php
+    @$addingstudent = $_POST["addingstudent"];
+    @$studentlogin = $_POST["studentlogin"];
+    @$studentpasswd = $_POST["studentpasswd"];
+    if (!empty($studentlogin) && !empty($studentpasswd)){
+        mysqli_query($polaczenie, "Insert into student(parent_id, username, passwd) values ('$addingstudent', '$studentlogin', '$studentpasswd')");
+    }
+    else{
+        echo "Pola nie moga byc puste!";
+    }
+    ?>
 <table>
     <tr>
         <th>
@@ -186,7 +266,24 @@
     @$studentnumber = $_POST["studentnumber"];
     mysqli_query($polaczenie, "Delete from student where id = '$studentnumber';");
     ?>
-    <h3>Usuń Rodzica: </h3>
+    <h3>Dodaj / Usuń Rodzica: </h3>
+    <form method="post" action="AdminPanel.php">
+        <label for="parentlogin">Login:</label>
+        <input type="text" id="parentlogin" name="parentlogin"><br />
+        <label for="parentpasswd">Hasło:</label>
+        <input type="password" id="parentpasswd" name="parentpasswd">
+        <input type="submit" value="Dodaj">
+    </form>
+    <?php
+    @$parentlogin = $_POST["parentlogin"];
+    @$parentpasswd = $_POST["parentpasswd"];
+    if (!empty($studentlogin) && !empty($studentpasswd)){
+        mysqli_query($polaczenie, "insert into users(username, passwd) values ('$parentlogin', '$parentpasswd')");
+    }
+    else{
+        echo "Pola nie moga byc puste!";
+    }
+    ?>
     <table>
         <tr>
             <th>
@@ -211,32 +308,6 @@
     <?php
     @$parentnumber = $_POST["parentnumber"];
     mysqli_query($polaczenie, "Delete from users where id = '$parentnumber';");
-    ?>
-    <h3>Usuń kategorię: </h3>
-    <table>
-        <tr>
-            <th>
-                ID
-            </th>
-            <th>
-                Nazwa
-            </th>
-        </tr>
-        <?php
-        $zapytanie = $polaczenie->query("SELECT id, name from category;");
-        while ($row = mysqli_fetch_array($zapytanie)){
-            echo "<tr>" . "<td>" . $row[0] . "</td>" . "<td>" . $row[1] . "</td>" . "</tr>";
-        }
-        ?>
-    </table>
-    <form action="AdminPanel.php" METHOD="post">
-        <label for="categorynumber">Identyfikator Kategorii do usunięcia: </label>
-        <input type="number" name="categorynumber" id="categorynumber">
-        <input type="submit" value="usuń">
-    </form>
-    <?php
-    @$categorynumber = $_POST["categorynumber"];
-    mysqli_query($polaczenie, "Delete from category where id = '$categorynumber';");
     ?>
     </body>
 </html>
