20f34e50ede158830f4df1d63fae24cef34e364f
adwokat2115 <brunomaniak2008@gmail.com>
2024-06-13 21:48:36 +0200

    Update ParentPanel.php

--- Parent/ParentPanel.php
+++ Parent/ParentPanel.php
@@ -68,67 +68,6 @@
             ?>
         </section>
         <section id="slowka">
-            <h3>Dodaj Kategorie:</h3>
-                <form method="post" action="ParentPanel.php">
-                    <label for="db1">Uczeń:</label>
-                    <select id="db1" name="db1">
-                        <?php
-                        $zapytanie = $polaczenie->query("SELECT id, username from student where parent_id = $id;");
-                        while ($row = mysqli_fetch_array($zapytanie)){
-                            echo "<option value='$row[0]'>" . $row[1] . "</option>";
-                        }
-                        ?>
-                    </select><br />
-                    <label for="category_name">Nazwa:</label>
-                    <input type="text" id="category_name" name="category_name"> <br>
-                    <input type="submit" value="Dodaj">
-                </form>
-            <?php
-                @$student = $_POST['db1'];
-                @$category_name = $_POST['category_name'];
-                if (!empty($word_pl) && !empty($word_en)) {
-                    @mysqli_query($polaczenie, "INSERT INTO category(parent_id, student_id,name) values('$id','$student','$category_name');");
-                }
-            ?>
-            <h3>Dodaj słówka:</h3>
-            <form method="post" action="ParentPanel.php" >
-                <label for="db">Uczeń:</label>
-                <select id="db" name="db">
-
-                    <?php
-                    $zapytanie = $polaczenie->query("SELECT id, username from student where parent_id = $id;");
-                    while ($row = mysqli_fetch_array($zapytanie)){
-                        echo "<option value='$row[0]'>" . $row[1] . "</option>";
-                    }
-                    ?>
-                </select><br />
-                <label for="category">Wybierz kategorie:</label>
-                <select id="category" name="category">
-                    <?php
-                    $zapytanie = $polaczenie->query("SELECT id, name from category where parent_id = $id;");
-                    while ($row = mysqli_fetch_array($zapytanie)){
-                        echo "<option value='$row[0]'>" . $row[1] . "</option>";
-                    }
-                    ?>
-                </select><br />
-                <label for="word_en">Angieslkie słówko:</label>
-                <input type="text" id="word_en" name="word_en"><br />
-                <label for="word_pl">Polskie słówko</label>
-                <input type="text" id="word_pl" name="word_pl"><br />
-                <label for="definition">Definicja:</label>
-                <input type="text" id="definition" name="definition"><br />
-                <input type="submit" value="Dodaj">
-            </form>
-            <?php
-            @$student = $_POST["db"];
-            @$category = $_POST["category"];
-            @$word_en = $_POST["word_en"];
-            @$word_pl = $_POST["word_pl"];
-            @$definition = $_POST["definition"];
-            if (!empty($word_pl) && !empty($word_en)) {
-                @mysqli_query($polaczenie, "Insert into words(category_id, parent_id, student_id, word_en, word_pl, description) values ('$category','$id','$student','$word_en','$word_pl' ,'$definition');");
-            }
-            ?>
             <h3>Dodane słówka:</h3>
             <form method="post" action="ParentPanel.php" >
                 <select id="category" name="category1" onchange="this.form.submit();">
@@ -187,6 +126,77 @@
                 }
                 ?>
             </table>
+        </section>
+        <section id="dodawanie">
+            <h3>Dodaj dziecko:</h3>
+            <form action="ParentPanel.php" method="post">
+
+            </form>
+            <h3>Dodaj Kategorie:</h3>
+            <form method="post" action="ParentPanel.php">
+                <label for="db1">Uczeń:</label>
+                <select id="db1" name="db1">
+                    <?php
+                    $zapytanie = $polaczenie->query("SELECT id, username from student where parent_id = $id;");
+                    while ($row = mysqli_fetch_array($zapytanie)){
+                        echo "<option value='$row[0]'>" . $row[1] . "</option>";
+                    }
+                    ?>
+                </select><br />
+                <label for="category_name">Nazwa:</label>
+                <input type="text" id="category_name" name="category_name"> <br>
+                <input type="submit" value="Dodaj">
+            </form>
+            <?php
+            @$student = $_POST['db1'];
+            @$category_name = $_POST['category_name'];
+            if (!empty($category_name)) {
+                @mysqli_query($polaczenie, "INSERT INTO category(parent_id, student_id,name) values('$id','$student','$category_name');");
+            }
+            ?>
+            <h3>Dodaj słówka:</h3>
+            <form method="post" action="ParentPanel.php" >
+                <label for="db">Uczeń:</label>
+                <select id="db" name="db" onchange="this.form.submit()">
+                    <option value="0">wybierz ucznia</option>
+                    <?php
+                    $zapytanie = $polaczenie->query("SELECT id, username from student where parent_id = $id;");
+                    while ($row = mysqli_fetch_array($zapytanie)){
+                        echo "<option value='$row[0]'>" . $row[1] . "</option>";
+                    }
+                    ?>
+                </select><br />
+            </form>
+            <?php
+            @$student_id = $_POST["db"];
+            ?>
+            <form method="post" action="ParentPanel.php">
+                <label for="category">Wybierz kategorie:</label>
+                <select id="category" name="category">
+                    <?php
+                    $zapytanie = $polaczenie->query("SELECT id, name from category where parent_id = $id and student_id = $student_id;");
+                    while ($row = mysqli_fetch_array($zapytanie)){
+                        echo "<option value='$row[0]'>" . $row[1] . "</option>";
+                    }
+                    ?>
+                </select><br />
+                <label for="word_en">Angieslkie słówko:</label>
+                <input type="text" id="word_en" name="word_en"><br />
+                <label for="word_pl">Polskie słówko</label>
+                <input type="text" id="word_pl" name="word_pl"><br />
+                <label for="definition">Definicja:</label>
+                <input type="text" id="definition" name="definition"><br />
+                <input type="submit" name="submit" value="Dodaj">
+            </form>
+            <?php
+            @$category = $_POST['category'];
+            @$word_en = $_POST['word_en'];
+            @$word_pl = $_POST['word_pl'];
+            @$definition = $_POST['definition'];
+            if (!empty($word_pl) && !empty($word_en)) {
+                @mysqli_query($polaczenie, "Insert into words(category_id, parent_id, student_id, word_en, word_pl, description) values ('$category','$id','$student_id','$word_en','$word_pl' ,'$definition');");
+            }
+            ?>
         </section>
     </main>
 </body>
